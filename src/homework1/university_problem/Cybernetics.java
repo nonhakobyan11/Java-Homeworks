a169ed33063f1f9ed034f8f93b160b228143d74c
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-12 20:52:24 +0400

    Adding implementations of interface Faculty

---
+++ src/homework1/university_problem/Cybernetics.java
@@ -0,0 +1,37 @@
+package homework1.university_problem;
+
+import java.util.ArrayList;
+
+public class Cybernetics implements FacultyFactory.Faculty {
+
+
+    @Override
+    public void setArrayOfGroupsInTheFaculty() {
+
+    }
+
+    @Override
+    public void setStudentListForGroups(int option) {
+
+    }
+
+    @Override
+    public void setSubjectsListForGroups(int option) {
+
+    }
+
+    @Override
+    public ArrayList<String> getArrayOfGroupsInTheFaculty() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getStudentListGroup() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getSubjectsԼistForGroup() {
+        return null;
+    }
+}

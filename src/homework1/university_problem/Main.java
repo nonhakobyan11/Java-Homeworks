ce7cd467682aa6ee810479c99fc5bc708156d49e
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-12 21:31:33 +0400

    Main class is complete. All functions are invoked. All exceptions are handled.

--- src/homework1/university_problem/Main.java
+++ src/homework1/university_problem/Main.java
@@ -1,7 +1,10 @@
 package homework1.university_problem;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 
 public class Main {
@@ -10,6 +13,7 @@ public class Main {
         int optionFaculty;
         int optionGroup;
         int optionStudent;
+        int optionSubject;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -39,7 +43,7 @@ public class Main {
         System.out.println("\nBelow are mentioned all students full names in the group. Choose yours.");
         dekanat.printArray(studentsOfChosenGroup);
         optionStudent = scanner.nextInt();
-        if(optionStudent >= studentsOfChosenGroup.size()) {
+        if(optionStudent > studentsOfChosenGroup.size()) {
             throw new IndexOutOfBoundsException("There is no student with this name in your Group.");
         }
         System.out.println("Hi, " + studentsOfChosenGroup.get(optionStudent - 1) + "!");
@@ -49,6 +53,41 @@ public class Main {
         ArrayList<String> subjectsOfChosenGroup = chosenFaculty.getSubjectsListForGroup();
         System.out.println("\nBelow are mentioned all subjects that you are studying.");
         dekanat.printArray(subjectsOfChosenGroup);
+
+        System.out.println("\nSo, " + studentsOfChosenGroup.get(optionStudent - 1) + ", which subject's GPA do you want to know?");
+        optionSubject = scanner.nextInt();
+        if(optionSubject > subjectsOfChosenGroup.size()) {
+            throw new IndexOutOfBoundsException("There is no subject with this name in your Group.");
+        }
+        System.out.println("Please, enter your grades from '" + subjectsOfChosenGroup.get(optionSubject - 1) + "' subject.");
+        dekanat.calculateAverageMarkForOneStudent();
+
+        // View more options that can interest you
+        studentsOfChosenGroup.clear();
+        subjectsOfChosenGroup.clear();
+
+        while(true) {
+            System.out.println("\nAlso you can get average grade of this subject for:");
+            System.out.println("1. All group\n2. All faculty\n3. All university\n\nEnter '4' if you want to exit program.");
+            int optionAverage = scanner.nextInt();
+            switch (optionAverage) {
+                case 1:
+                    dekanat.calculateAverageMarkForGroup(chosenFaculty, optionGroup, optionStudent);
+                    break;
+                case 2:
+                    dekanat.calculateAverageMarkForFaculty(chosenFaculty, optionStudent);
+                    break;
+                case 3:
+                    dekanat.calculateAverageGradeForUniversity(factory.getFaculty(1), factory.getFaculty(2), optionStudent);
+                    break;
+                case 4:
+                    System.out.println("We hope you are enjoying your study in our University.");
+                    exit(0);
+                default:
+                    System.out.println("Try again.");
+                    throw new InputMismatchException("There is no such option.");
+            }
+        }
 
     }
 }

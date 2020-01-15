bdc25e77426af83a873d3dcbc0f066d8844f159d
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-15 11:01:46 +0400

    Adding Subject class and Student class (using Builder pattern)

--- src/homework1/university_problem/Student.java
+++ src/homework1/university_problem/Student.java
@@ -1,4 +1,74 @@
 package homework1.university_problem;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String universityName;
+    private String facultyName;
+    private String groupName;
+    private Subject[] subjects;
+
+    public Student(StudentBuilder builder) {
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+        email = builder.email;
+        universityName = builder.universityName;
+        facultyName = builder.facultyName;
+        groupName = builder.groupName;
+    }
+
+    public void setSubjects(Subject[] subjects) {
+        if (subjects.length == 0) {
+            throw new RuntimeException("Student has no subject");
+        }
+        this.subjects = subjects;
+    }
+
+    public Subject[] getSubjects() {
+        return subjects;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public static class StudentBuilder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String universityName;
+        private String facultyName;
+        private String groupName;
+
+        public StudentBuilder(String firstName, String lastName, String email, String universityName, String facultyName, String groupName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.universityName = universityName;
+            this.facultyName = facultyName;
+            this.groupName = groupName;
+        }
+
+        public Student build() {
+            Student student = new Student(this);
+            return student;
+        }
+    }
 }
--- src/homework1/university_problem/Subject.java
+++ src/homework1/university_problem/Subject.java
@@ -1,4 +1,26 @@
 package homework1.university_problem;
 
 public class Subject {
+    private String subjectName;
+    private int grade;
+
+    public Subject(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public void setGrade(int grade)  {
+        if (grade < 0 || grade > 10) {
+            System.out.println("Grade should be more or equal to zero and less or equal to 10. Please fix it.");
+            throw new RuntimeException("Invalid grade");
+        }
+        this.grade = grade;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
 }

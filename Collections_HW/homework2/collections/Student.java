a9888339d6e40fe69091ebc6d1e44af427d1a811
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-22 13:15:41 +0400

    Adding Student class

---
+++ Collections_HW/homework2/collections/Student.java
@@ -0,0 +1,32 @@
+package homework2.collections;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "First Name: " + firstName +
+                ", Last Name: " + lastName +
+                ", Age: " + age;
+    }
+}

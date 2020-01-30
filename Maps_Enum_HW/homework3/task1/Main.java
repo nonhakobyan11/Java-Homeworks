7664498d9a91d9fecb2f80203d34d8b4274b78e4
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-31 01:58:38 +0400

    Task 1 - a) is done

--- Maps_Enum_HW/homework3/task1/Main.java
+++ Maps_Enum_HW/homework3/task1/Main.java
@@ -1,7 +1,15 @@
 package homework3.task1;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // a)
+        StudentService service = new StudentService();
+        List<Student> myStudentList = service.initialize();
+        HashMap<Student, Integer> myStudentMap = service.getStudentMap(myStudentList);
+        service.printStudentMap(myStudentMap);
 
     }
 }

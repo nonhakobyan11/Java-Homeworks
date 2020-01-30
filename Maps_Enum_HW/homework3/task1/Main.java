c7a1e52e8c2b9d2fbe41882eae57fa6a263ca4e5
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-31 00:50:28 +0400

    Starting new homework

---
+++ Maps_Enum_HW/homework3/task1/Main.java
@@ -0,0 +1,7 @@
+package homework3.task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Maps_Enum_HW/homework3/task2/Main.java
@@ -0,0 +1,7 @@
+package homework3.task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

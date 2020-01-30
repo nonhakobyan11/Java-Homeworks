1eb05a1a2a9bd25fd85dc1778c5dcde547912a10
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-31 02:28:05 +0400

    Changing some variables names. Adding the following methods: getFacultiesMap(), printFacultiesMap()

--- Maps_Enum_HW/homework3/task1/StudentService.java
+++ Maps_Enum_HW/homework3/task1/StudentService.java
@@ -1,36 +1,53 @@
 package homework3.task1;
 
+import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class StudentService {
-    private HashMap<Student, Integer> studentMap = new HashMap<>();
+    private Map<Student, Integer> studentsMap = new HashMap<>();
+    private Map<Faculty, Integer> facultiesMap = new HashMap<>();
 
     public List<Student> initialize(){
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Diana", "Asatryan", 22, "098111111", "Math"));
-        students.add(new Student("Diana", "Asatryan", 32, "097121411", "Physics"));
-        students.add(new Student("Diana", "Asatryan", 21, "093811171", "Philosophy"));
-        students.add(new Student("Karen", "Balayan", 18, "098111781", "Math"));
-        students.add(new Student("Karen", "Balayan", 23, "098188751", "Philosophy"));
-        students.add(new Student("Elen", "Mirzoyan", 28, "098145811", "English"));
+        students.add(new Student("Diana", "Asatryan", 22, "098111111", Faculty.MATH));
+        students.add(new Student("Diana", "Asatryan", 32, "097121411", Faculty.PHYSICS));
+        students.add(new Student("Diana", "Asatryan", 21, "093811171", Faculty.PHILOSOPHY));
+        students.add(new Student("Karen", "Balayan", 18, "098111781", Faculty.MATH));
+        students.add(new Student("Karen", "Balayan", 23, "098188751", Faculty.PHILOSOPHY));
+        students.add(new Student("Elen", "Mirzoyan", 28, "098145811", Faculty.ENGLISH));
         return students;
     }
 
     // key - student, value - countOfOccurrences
-    public HashMap<Student, Integer> getStudentMap(List<Student> students) {
+    public Map<Student, Integer> getStudentsMap(List<Student> students) {
         for (Student s : students) {
-            Integer countOfOccurrences = studentMap.get(s);
-            studentMap.put(s, (countOfOccurrences == null) ? 1 : countOfOccurrences + 1);
+            Integer countOfOccurrences = studentsMap.get(s);
+            studentsMap.put(s, (countOfOccurrences == null) ? 1 : countOfOccurrences + 1);
         }
-        return studentMap;
+        return studentsMap;
     }
 
-    public void printStudentMap(HashMap<Student, Integer> hashMap) {
+    public void printStudentMap(Map<Student, Integer> hashMap) {
         System.out.println("\nNumbers of occurrences of the same first and last names:");
         for (HashMap.Entry<Student, Integer> entry : hashMap.entrySet()) {
-            System.out.println(entry.getKey().toString() + " " + entry.getValue());
+            System.out.println(entry.getKey().toString() + " - " + entry.getValue());
+        }
+    }
+
+    public Map<Faculty, Integer> getFacultiesMap(List<Student> students) {
+        for (Student s : students) {
+            Integer studentsCountInFaculties = facultiesMap.get(s.getFacultyName());
+            facultiesMap.put(s.getFacultyName(), (studentsCountInFaculties == null) ? 1 : studentsCountInFaculties + 1);
+        }
+        return facultiesMap;
+    }
+
+    public void printFacultiesMap(Map<Faculty, Integer> hashMap) {
+        System.out.println("\nNumber of students in each faculty is:");
+        for (HashMap.Entry<Faculty, Integer> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 

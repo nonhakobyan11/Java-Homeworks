6f8e1689ec0bd6ffe4dc2174d2842213c89e307e
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-01-31 04:14:23 +0400

    Task2 is done

--- Maps_Enum_HW/homework3/task2/Main.java
+++ Maps_Enum_HW/homework3/task2/Main.java
@@ -2,6 +2,15 @@ package homework3.task2;
 
 public class Main {
     public static void main(String[] args) {
+        Service service = new Service();
+        service.traverseAndPrintAllMonthNames();
+        System.out.println();
+        System.out.println(Month.APRIL + "'s holidays are " + Month.APRIL.getHolidays());
+        System.out.println(service.getDaysInMonth(Month.FEBRUARY, 2020));
+        System.out.println(service.getDaysInMonth(Month.MARCH, 2020));
+        System.out.println(service.getNumberOfHolidaysInMonth(Month.JANUARY));
+        System.out.println(service.checkHolidayOrNot(Month.MARCH, 8));
+        System.out.println(service.checkHolidayOrNot(Month.MARCH, 21));
 
     }
 }

ef22c89b230f008cf646a97d6b68f08b54701ed2
Nonna Hakobyan <nonhakobyan11@gmail.com>
2020-02-11 22:27:49 +0400

    Some changes

--- Threads/homework5/task2/User.java
+++ Threads/homework5/task2/User.java
@@ -1,40 +1,5 @@
 package homework5.task2;
 
-//public class User {
-//    private String name;
-//    private boolean b = false;
-//
-//    public User(String name) {
-//        this.name = name;
-//    }
-//
-//    public boolean isB() {
-//        return b;
-//    }
-//
-//    public void setB(boolean b) {
-//        this.b = b;
-//    }
-//
-//    public synchronized void printName() {
-//        if (isB()) {
-//            setB(true);
-//            try {
-//                wait();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        } else if (isB()){
-//            notify();
-//        }
-//        System.out.println(Thread.currentThread().getName() + " " + getName());
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//}
-
 public class User {
 
     public synchronized void print(String name){
@@ -44,7 +9,6 @@ public class User {
             this.wait();
         }catch (InterruptedException e){}
     }
-
 }
 
 
